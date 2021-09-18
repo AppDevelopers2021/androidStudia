711432d5772e4f1936c2abd9996718481813e4e1
HyunwooLim <lhwdev2008@gmail.com>
2021-09-18 16:56:58 +0900

    MainActivity Intro Set 20210918

--- app/src/main/java/app/web/studia_kr/MainActivity.java
+++ app/src/main/java/app/web/studia_kr/MainActivity.java
@@ -2,13 +2,30 @@ package app.web.studia_kr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import app.web.studia_kr.ui.login.LoginActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    Timer introtimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        introtimer = new Timer();
+        introtimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                finish();
+            }
+        }, 2000);
     }
 }
