9aed779b9312e79e67581d81ade6615e3277bfda
HyunwooLim <lhwdev2008@gmail.com>
2021-09-18 13:35:56 +0900

    Add Activities 20210918

---
+++ app/src/main/java/app/web/studia_kr/data/model/LoggedInUser.java
@@ -0,0 +1,23 @@
+package app.web.studia_kr.data.model;
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+public class LoggedInUser {
+
+    private String userId;
+    private String displayName;
+
+    public LoggedInUser(String userId, String displayName) {
+        this.userId = userId;
+        this.displayName = displayName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
