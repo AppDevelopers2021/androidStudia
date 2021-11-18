48a5639f151131419934fc243f32ba5cc87e816d
HyunwooLim <lhwdev2008@gmail.com>
2021-11-18 22:07:56 +0900

    Colorful Subject Space Update

--- app/src/main/java/app/web/studia_kr/CustomAdapter.java
+++ app/src/main/java/app/web/studia_kr/CustomAdapter.java
@@ -4,13 +4,12 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -37,7 +36,162 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
     @Override
     public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
         holder.content.setText(arrayList.get(position).getContent());
-        subject = arrayList.get(position).getSubject();
+        holder.subject.setText(arrayList.get(position).getSubject());
+
+        if (arrayList.get(position).getSubject().toString() == "가정") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglered) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglered));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "과학") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectangleblue) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectangleblue));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "국어") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglelightblue) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglelightblue));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "기술") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglegreen) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglegreen));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "도덕") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglepurple) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglepurple));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "독서") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectangleyellow) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectangleyellow));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "미술") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglelightgreen) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglelightgreen));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "보건") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglegray) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglegray));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "사회") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectangleblue) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectangleblue));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "수학") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglegreen) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglegreen));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "영어") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectangleyellow) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectangleyellow));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "음악") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglegreen) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglegreen));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "정보") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglered) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglered));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "진로") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglegray) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglegray));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "창체") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglepurple) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglepurple));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "체육") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglelightgreen) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglelightgreen));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "환경") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglelightblue) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglelightblue));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "자율") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglered) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglered));
+            }
+        }
+        if (arrayList.get(position).getSubject().toString() == "기타") {
+            final int sdk = android.os.Build.VERSION.SDK_INT;
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                holder.constraintLayout.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.rectanglegray) );
+            } else {
+                holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglegray));
+            }
+        }
+
+
 
         holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
@@ -58,12 +212,14 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
 
         TextView subject;
         TextView content;
+        ConstraintLayout constraintLayout;
 
         public CustomViewHolder(@NonNull View itemView) {
             super(itemView);
 
             this.subject = itemView.findViewById(R.id.tvSubject);
             this.content = itemView.findViewById(R.id.tvContent);
+            this.constraintLayout = (ConstraintLayout) itemView.findViewById(R.id.layout);
         }
     }
 
