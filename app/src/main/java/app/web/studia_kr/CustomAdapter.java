ede964f4b55cd2ef7426f61b9d01984b471784db
HyunwooLim <lhwdev2008@gmail.com>
2021-11-27 14:58:40 +0900

    Colorful Subject Display Layout

--- app/src/main/java/app/web/studia_kr/CustomAdapter.java
+++ app/src/main/java/app/web/studia_kr/CustomAdapter.java
@@ -36,6 +36,8 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_view, parent, false);
         CustomViewHolder holder = new CustomViewHolder(view);
 
+        context = parent.getContext();
+
         return holder;
     }
 
@@ -44,15 +46,44 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
         holder.content.setText(arrayList.get(position).getContent());
         holder.subject.setText(arrayList.get(position).getSubject());
 
-        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                //Delete
-                remove(holder.getAdapterPosition());
-
-                return true;
-            }
-        });
+        if (arrayList.get(position).getSubject() == "가정")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectangleblue));
+        if (arrayList.get(position).getSubject() == "과학")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglegray));
+        if (arrayList.get(position).getSubject() == "국어")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglegreen));
+        if (arrayList.get(position).getSubject() == "기술")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglelightblue));
+        if (arrayList.get(position).getSubject() == "도덕")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglelightgreen));
+        if (arrayList.get(position).getSubject() == "독서")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglepurple));
+        if (arrayList.get(position).getSubject() == "미술")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglered));
+        if (arrayList.get(position).getSubject() == "보건")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectangleyellow));
+        if (arrayList.get(position).getSubject() == "사회")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectangleblue));
+        if (arrayList.get(position).getSubject() == "수학")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglegray));
+        if (arrayList.get(position).getSubject() == "영어")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglegreen));
+        if (arrayList.get(position).getSubject() == "음악")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglelightblue));
+        if (arrayList.get(position).getSubject() == "정보")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglelightgreen));
+        if (arrayList.get(position).getSubject() == "진로")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglepurple));
+        if (arrayList.get(position).getSubject() == "창체")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglered));
+        if (arrayList.get(position).getSubject() == "체육")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectangleyellow));
+        if (arrayList.get(position).getSubject() == "환경")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectangleblue));
+        if (arrayList.get(position).getSubject() == "자율")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglegray));
+        if (arrayList.get(position).getSubject() == "기타")
+            holder.constraintLayout.setBackground(ContextCompat.getDrawable(context, R.drawable.rectanglegreen));
     }
 
     @Override
@@ -72,16 +103,6 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
             this.subject = itemView.findViewById(R.id.tvSubject);
             this.content = itemView.findViewById(R.id.tvContent);
             this.constraintLayout = (ConstraintLayout) itemView.findViewById(R.id.layout);
-        }
-    }
-
-    public void remove(int position) {
-        try {
-            arrayList.remove(position);
-            notifyItemRemoved(position);
-        }
-        catch (IndexOutOfBoundsException ex) {
-            ex.printStackTrace();
         }
     }
 }
