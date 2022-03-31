0893b34d59b7c9f883b3f5636e309e2aff0e2745
HyunwooLim <lhwdev2008@gmail.com>
2022-03-31 22:51:16 +0900

    fix: Fix NullPointerError caused in NotificationService.java

--- app/src/main/java/app/web/studia_kr/backgroundservice/NotificationService.java
+++ app/src/main/java/app/web/studia_kr/backgroundservice/NotificationService.java
@@ -1,7 +1,6 @@
 package app.web.studia_kr.backgroundservice;
 
 import android.app.IntentService;
-import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
 
@@ -30,33 +29,37 @@ public class NotificationService extends IntentService {
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-        FirebaseDatabase.getInstance().getReference()
-                .child("calendar").child(FirebaseAuth.getInstance().getUid())
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.hasChild(new SimpleDateFormat("yyyyMMdd").format(Calendar.getInstance())))
-                    isTodayScheduleExists = true;
-                else
-                    isTodayScheduleExists = false;
+        if (FirebaseAuth.getInstance() != null) {
+            FirebaseDatabase.getInstance().getReference()
+                    .child("calendar").child(FirebaseAuth.getInstance().getUid())
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            String date = new SimpleDateFormat("yyyyMMdd").format(Calendar.getInstance().getTime());
+
+                            if (snapshot.hasChild(date))
+                                isTodayScheduleExists = true;
+                            else
+                                isTodayScheduleExists = false;
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            isTodayScheduleExists = false;
+                        }
+                    });
+
+            if (isTodayScheduleExists) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "StudiaNotify")
+                        .setSmallIcon(R.drawable.iconstudia)
+                        .setContentTitle("스튜디아에서 오늘 일정을 확인하세요!")
+                        .setContentText("오늘도 스튜디아와 함께해요 ❤")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setAutoCancel(true)
+                        .setContentIntent(PendingIntent.getActivity(this, 2, new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT));
+
+                NotificationManagerCompat.from(this).notify(3, builder.build());
             }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                isTodayScheduleExists = false;
-            }
-        });
-
-        if (isTodayScheduleExists) {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "StudiaNotify")
-                    .setSmallIcon(R.drawable.iconstudia)
-                    .setContentTitle("스튜디아에서 오늘 일정을 확인하세요!")
-                    .setContentText("오늘도 스튜디아와 함께해요 ❤")
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                    .setAutoCancel(true)
-                    .setContentIntent(PendingIntent.getActivity(this, 2, new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT));
-
-            NotificationManagerCompat.from(this).notify(3, builder.build());
         }
     }
 }
